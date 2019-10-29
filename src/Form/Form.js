09ea9bbadf5e3fd434ac0e1c7bda698be1a83c6d
home <andybaaall>
2019-10-29 16:05:36 +1300

    adding a new item every time the form changes (ie every keystroke)

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,13 +4,48 @@ import Button from '../Button/Button';
 import '../Button/Button.css';
 
 class Form extends Component {
+    // check out commit 89d41650190401821b780f1991274143af4dbfd3 for notes on binding ...
+    // ... es5, es6, and not binding.
+    submitForm = (e) => {
+        e.preventDefault();
+    }
+
+    // bound functions go inside the constructor (89d41650190401821b780f1991274143af4dbfd3, es5/6)
+    constructor(props){
+        super(props);
+
+        this.changeInput = this.changeInput.bind(this);
+    }
+
+    changeInput(e){
+        // console.log(e.target.value);
+        // now that we've got the event target's value, we need to set the value property
+        // of our Form Component to that value
+
+        this.props.changeInputValue(e.target.value)
+        // this is the handleChangeInputValue function List.js
+        // we're calling this component's property 'changeInputValue'
+
+        this.props.submit(e.target.value)
+        // (this is where the one-new-item-per-keystroke bug comes from)
+        // this is the handleSubmitForm function, defined in List.js
+        // we're calling this component's property 'submit'
+    }
+
+    submitForm(e){
+        // ??
+    }
+
     render(){
+        // because we can't access the DOM in React, we need to define our functions inline.
+
+            // <Button btnText="" btnType="redundant button should probably throw an err, eh?"/>
+            // <Button btnText="Find an Item" btnType="greyBtn"/>
+
         return(
-            <form className="searchForm">
-                <input className="searchBar"/>
+            <form className="searchForm" onSubmit={this.submitForm}>
+                <input className="searchBar" value={this.props.item.value} onChange={this.changeInput}/>
                 <Button btnText="Add new Item"/>
-                <Button btnText="" btnType="redundant button should definitely throw an err"/> 
-                <Button btnText="Find an Item" btnType="greyBtn"/>
             </form>
         )
     }
--- src/List/List.js
+++ src/List/List.js
@@ -25,38 +25,85 @@ class List extends Component {
                 id: 3,
                 text: 'item three'
             }
-            ]
+        ], currentItem: {
+            id: null,
+            value: ''
         }
+
+    }
+}
+
+handleChangeInputValue = (text) => {
+    // console.log('this input value will come from Form.js');
+    let {currentItem} = this.state;
+    // remember 'let {km} = car;', which outputs // km = 178000
+    // ... or 'let {manufacturer} = car;', which outputs // manufacturer = 'honda'
+    // basically this creates a var called currentItem, whose value is init as ''
+
+    currentItem.value = text;
+    this.setState({
+        currentItem: currentItem
+    })
+}
+
+handleSubmitForm = () => {
+    console.log('form submitted!');
+
+    let {allItems, currentItem} = this.state;
+
+    const newItem = {
+        id: allItems.length + 1,
+        text: currentItem.value
     }
 
-    // ... and a render()
-    render(){
-        // if we ever need to do any vanilla JS stuff, like a console.log(),
-        // we need to do it before the return()
-        console.log(this.state.allItems);
+    allItems.push(newItem);
 
-        return(
-            <div className="ListContainer">
-                <h1>shopping list</h1>
-                <Form />
-                <div className="List">
-                    {   // curly brackets indicate to react that we're going to be writing logic
-                        // each iteration of the loop will be known as singleItem
-                        this.state.allItems.map(singleItem => {
-                            // return <Item/> - this gets us 3 x Item One
-                            return <Item
-                                key = {singleItem.id}
-                                itemInfo = {singleItem}
-                                // so: each object in the array allItems (singleItem) has a props object.
-                                // each props object has an itemInfo property/value pair.
-                                // each itemInfo object has a text property/value pair. 
-                                />
-                        })
-                    }
-                </div>
-            </div>
-        )
-    }
+    this.setState({
+        currentItem: {
+            id: null,
+            value: ''
+        },
+        allItems: allItems
+    });
+}
+
+
+// ... and a render()
+render(){
+    // if we ever need to do any vanilla JS stuff, like a console.log(),
+    // we need to do it before the return()
+    console.log(this.state.allItems);
+
+    return(
+        <div className="ListContainer">
+        <h1>shopping list</h1>
+        <Form
+        item = {this.state.currentItem}
+        changeInputValue = {this.handleChangeInputValue}
+        // so the form has a property, which is a function, which is ...
+        // ... handleChangeInputValue()
+        submit = {this.handleSubmitForm}
+        // so the form has yet another property, which is a function, which is ...
+        // ... handleSubmitForm()
+        />
+        <div className="List">
+        {   // curly brackets indicate to react that we're going to be writing logic
+            // each iteration of the loop will be known as singleItem
+            this.state.allItems.map(singleItem => {
+                // return <Item/> - this gets us 3 x Item One
+                return <Item
+                key = {singleItem.id}
+                itemInfo = {singleItem}
+                // so: each object in the array allItems (singleItem) has a props object.
+                // each props object has an itemInfo property/value pair.
+                // each itemInfo object has a text property/value pair.
+                />
+            })
+        }
+        </div>
+        </div>
+    )
+}
 }
 
 export default List;
